ef253404947c37f3057e2f4cdba7cd2028fdcf06
codemsbr <mi7pum53j@code.edu.az>
2023-11-02 18:10:06 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace Task13_Reflection_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            FieldInfo[] fieldInfos = user.GetType().GetFields(BindingFlags.NonPublic |
+                         BindingFlags.Instance);
+
+            fieldInfos[0].SetValue(user,12);
+            fieldInfos[1].SetValue(user,"asassas");
+
+            
+            var info = user.GetType().GetMethod("ChangeAge", BindingFlags.Static | BindingFlags.NonPublic);
+
+            object[] parameters = {12};
+            info.Invoke(user, parameters);
+
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task13_Reflection_
+{
+    internal class User
+    {
+        int Id;
+        string Name;
+        static int age;
+
+        static void ChangeAge(int _age)
+        {
+           age = _age;
+        }
+    }
+}
